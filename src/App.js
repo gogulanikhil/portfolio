2c3aeaba2935e0863cdd9ad46847afbe8c4e8249
nikhil <gogulanikhil@gamil.com>
2023-05-02 19:31:10 +0530

    portFolio delete method

--- src/App.js
+++ src/App.js
@@ -1,38 +1,65 @@
 import './App.css';
-
-
-  import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [profile, setProfile] = useState(null);
+  const [profiles, setProfiles] = useState([]);
 
   useEffect(() => {
     fetch('http://localhost:8080/get')
       .then(response => response.json())
       .then(data => {
-        setProfile(data);
+        setProfiles(data);
       })
       .catch(error => {
         console.error(error);
       });
-  }, []);
-  if (!profile || Object.keys(profile).length === 0) {
+  }, [profiles.length]);
+
+  if (!profiles || Object.keys(profiles).length === 0) {
     return <div>Loading...</div>;
   }
+  const removeById= async(id)=>{
+      // fetch(`http://localhost:8080/delete/${id}`,{
+      //   method:'DELETE'
+      // }).then(response => response.json())
+      //   .then(data=>{
+      //     console.log(data)
+      //     setProfiles(profiles.filter(eachObj => eachObj.id !== id));
+      //   })
+      //   .catch(error => {
+      //     console.error('There was a problem with the delete operation:',error);
+      //   });
+
+      const repsone = await fetch(`http://localhost:8080/delete/${id}`, {
+        method : 'DELETE'
+      })
+
+      const data = await repsone.json();
+      console.log(data)
+      setProfiles(profiles.filter(eachData => eachData.id !== id))
+
+    }
+
+    console.log(profiles)
+  // const edit=(id)=>{
+
+  // }
   return (
-    profile.map((eachObj)=>{
-      const{name,serialno,status,age,about,skills}=eachObj
-      return (<div>
+    profiles.map((eachObj) => {
+      const {id,name, serialno, status, age, about, skills ,index} = eachObj
+      return (<div key={index}>
         <h1>{name}</h1>
+        <p>id : {id}</p>
         <p>Serial No: {serialno}</p>
         <p>Age: {age}</p>
         <p>Status: {status}</p>
         <p>About: {about}</p>
         <p>Skills: {skills}</p>
+        {/* <button onClick={()=>{edit}}>edit</button> */}
+        <button onClick={()=>(removeById(id))}>delete</button>
       </div>
       );
     })
   )
 }
-
 export default App;
