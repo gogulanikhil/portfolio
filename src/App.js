befbc823b4acfc8e4a3dcb1020292d3cfb7906ed
nikhil <gogulanikhil@gamil.com>
2023-05-02 16:47:03 +0530

    portFolio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
+
+  import React, { useState, useEffect } from 'react';
+
 function App() {
+  const [profile, setProfile] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:8080/get')
+      .then(response => response.json())
+      .then(data => {
+        setProfile(data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+  if (!profile || Object.keys(profile).length === 0) {
+    return <div>Loading...</div>;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    profile.map((eachObj)=>{
+      const{name,serialno,status,age,about,skills}=eachObj
+      return (<div>
+        <h1>{name}</h1>
+        <p>Serial No: {serialno}</p>
+        <p>Age: {age}</p>
+        <p>Status: {status}</p>
+        <p>About: {about}</p>
+        <p>Skills: {skills}</p>
+      </div>
+      );
+    })
+  )
 }
 
 export default App;
